8ad8e38ea954be91e0b2ee784507b51c1b673577
sayooj <sayooj.o12@gmail.com>
2025-08-01 15:13:32 +0530

    admin part added

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Country, City, TourPackage, PackageSchedule, Enquiry
 
-# Register your models here.
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'country']
+    list_filter = ['country']
+    search_fields = ['name']
+
+
+@admin.register(TourPackage)
+class TourPackageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'source_country', 'destination_country']
+    list_filter = ['source_country', 'destination_country']
+    search_fields = ['title', 'description']
+
+
+@admin.register(PackageSchedule)
+class PackageScheduleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'package', 'from_date', 'to_date', 'amount']
+    list_filter = ['package']
+    search_fields = ['title', 'description']
+
+
+@admin.register(Enquiry)
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'phone', 'related_schedule']
+    list_filter = ['related_schedule']
+    search_fields = ['name', 'email', 'message']
